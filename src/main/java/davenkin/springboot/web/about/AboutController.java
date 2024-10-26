c54a4f1145c566543077201a8f2c913afddd9b33
Teng Yun <davenkin@126.com>
2024-10-26 11:40:54 +0800

    update

--- src/main/java/davenkin/springboot/web/about/AboutController.java
+++ src/main/java/davenkin/springboot/web/about/AboutController.java
@@ -5,20 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.ZonedDateTime;
-
-import static java.time.ZonedDateTime.now;
+import java.time.Instant;
 
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
 public class AboutController {
-    private final ZonedDateTime deployTime = now();
+    private final Instant deployTime = Instant.now();
 
     @GetMapping(value = "/about")
     public QAboutInfo about() {
         String deployTime = this.deployTime.toString();
-
         return QAboutInfo.builder()
                 .deployTime(deployTime)
                 .build();

9ea106d637d0d8251dad83dd08eb25b2e5aeba66
Teng Yun <davenkin@126.com>
2024-10-30 00:25:25 +0800

    update

--- src/main/java/davenkin/springboot/web/common/AbstractDomainEventHandler.java
+++ src/main/java/davenkin/springboot/web/common/AbstractDomainEventHandler.java
@@ -6,17 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Slf4j
 public abstract class AbstractDomainEventHandler<T extends DomainEvent> implements DomainEventHandler<T> {
 
-  @Autowired
-  private ConsumingDomainEventDao consumingDomainEventDao;
+    @Autowired
+    private ConsumingDomainEventDao consumingDomainEventDao;
 
-  @Override
-  public void handle(T domainEvent) {
-    if (this.consumingDomainEventDao.checkNotConsumed(domainEvent)) {
-      doHandle(domainEvent);
-    } else {
-      log.warn("Domain event[{}] has already been consumed, skip.", domainEvent.getId());
+    @Override
+    public void handle(T domainEvent) {
+        if (this.consumingDomainEventDao.checkNotConsumed(domainEvent)) {
+            doHandle(domainEvent);
+        } else {
+            log.warn("Domain event[{}] has already been consumed previously, skip handling.", domainEvent.getId());
+        }
     }
-  }
 
-  protected abstract void doHandle(T domainEvent);
+    protected abstract void doHandle(T domainEvent);
 }
--- src/main/java/davenkin/springboot/web/common/ConsumingDomainEventDao.java
+++ src/main/java/davenkin/springboot/web/common/ConsumingDomainEventDao.java
@@ -1,22 +1,27 @@
 package davenkin.springboot.web.common;
 
+import com.mongodb.client.result.UpdateResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
+
+import static davenkin.springboot.web.common.Constants.MONGO_ID;
+import static org.springframework.data.mongodb.core.ReplaceOptions.replaceOptions;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class ConsumingDomainEventDao {
-  private final MongoTemplate mongoTemplate;
+    private final MongoTemplate mongoTemplate;
 
-  public boolean checkNotConsumed(DomainEvent domainEvent) {
-    try {
-      this.mongoTemplate.insert(new ConsumingDomainEvent(domainEvent));
-    } catch (Throwable t) {
-      return false;
+    public boolean checkNotConsumed(DomainEvent domainEvent) {
+        ConsumingDomainEvent consumingDomainEvent = new ConsumingDomainEvent(domainEvent);
+        Query query = query(where(MONGO_ID).is(consumingDomainEvent.getId()));
+        UpdateResult result = this.mongoTemplate.replace(query, consumingDomainEvent, replaceOptions().upsert());
+        return result.getMatchedCount() == 0;
     }
-    return true;
-  }
 }

8ed4b1c52cb200db0331d79e931aaf2b3bff1976
Yun Yun4 Teng <tengyun4@lenovo.com>
2024-10-30 15:19:03 +0800

    update

--- src/main/java/davenkin/springboot/web/common/AbstractDomainEventHandler.java
+++ src/main/java/davenkin/springboot/web/common/AbstractDomainEventHandler.java
@@ -6,17 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Slf4j
 public abstract class AbstractDomainEventHandler<T extends DomainEvent> implements DomainEventHandler<T> {
 
-    @Autowired
-    private ConsumingDomainEventDao consumingDomainEventDao;
+  @Autowired
+  private ConsumingDomainEventDao consumingDomainEventDao;
 
-    @Override
-    public void handle(T domainEvent) {
-        if (this.consumingDomainEventDao.checkNotConsumed(domainEvent)) {
-            doHandle(domainEvent);
-        } else {
-            log.warn("Domain event[{}] has already been consumed previously, skip handling.", domainEvent.getId());
-        }
+  @Override
+  public void handle(T domainEvent) {
+    if (this.isRepeatable() || this.consumingDomainEventDao.checkNotConsumed(domainEvent)) {
+      doHandle(domainEvent);
+    } else {
+      log.warn("Domain event[{}] has been consumed previously and its handler[{}] is not idempotent, skip handling.",
+          domainEvent.getId(), this.getClass().getSimpleName());
     }
+  }
 
-    protected abstract void doHandle(T domainEvent);
+  protected abstract void doHandle(T domainEvent);
 }
--- src/main/java/davenkin/springboot/web/common/DomainEventHandler.java
+++ src/main/java/davenkin/springboot/web/common/DomainEventHandler.java
@@ -1,5 +1,11 @@
 package davenkin.springboot.web.common;
 
 public interface DomainEventHandler<T extends DomainEvent> {
-    void handle(T domainEvent);
+
+  // Can this handler been safely invoked multiple times for a given a domain event
+  default boolean isRepeatable() {
+    return false;
+  }
+
+  void handle(T domainEvent);
 }

7a6ff102154f0a9a9b40d344d1605b2d83a189c3
Teng Yun <davenkin@126.com>
2024-11-09 17:05:13 +0800

    update

--- src/main/java/davenkin/springboot/web/common/changelog/SampleChangeLog.java
+++ src/main/java/davenkin/springboot/web/common/changelog/SampleChangeLog.java
@@ -4,8 +4,10 @@ import davenkin.springboot.web.user.domain.User;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+@Slf4j
 @ChangeUnit(id = "sampleChangeUnit", order = "001", author = "davenkin")
 public class SampleChangeLog {
 
@@ -17,5 +19,6 @@ public class SampleChangeLog {
     @RollbackExecution
     public void sampleRollbackException(MongoTemplate mongoTemplate) {
         // roll back
+        log.warn("Sample change log rolled back.");
     }
 }
